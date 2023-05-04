9ddb20d1985ffced154537d5d4a7bdca047fe2f5
merox-vlados <merox-vlados@mail.ru>
2023-05-04 17:51:57 +0300

    Initial commit

---
+++ src/main/java/MainWorker.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Director director = new Director();
+        Worker worker = (n) -> {
+            for(int i = 0; i < n; i++) {
+                System.out.println("Working...");
+            }
+            return "Success";
+        };
+
+        String result = director.force((worker),5);
+        System.out.println(result);
+    }
+}
